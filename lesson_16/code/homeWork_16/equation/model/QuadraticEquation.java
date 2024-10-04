c70b71622b3660eb2b7adb80c05c5bbc19a625ae
lutsd <lutsdm@gmail.com>
2024-10-04 10:49:20 +0200

    04.10.2024 homeWork lesson16 Equation

---
+++ lesson_16/code/homeWork_16/equation/model/QuadraticEquation.java
@@ -0,0 +1,57 @@
+package homeWork_16.equation.model;
+
+public class QuadraticEquation {
+
+    private double a;
+    private double b;
+    private double c;
+
+    public QuadraticEquation(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+
+    public void displayQuadraticEquation() {
+        System.out.printf("Quadratic Equation: %.2fx² + %.2fx + %.2f\n", a, b, c);
+    }
+
+    public double delta() {
+        return b * b - 4 * a * c;
+    }
+
+    public int quantityRoots() {
+        double discriminant = delta();
+        if (discriminant > 0) {
+            return 2;
+        } else if (discriminant == 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
