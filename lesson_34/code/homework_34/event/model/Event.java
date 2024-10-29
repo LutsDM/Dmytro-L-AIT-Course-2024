92f1a618de9e7800c6c47abb79b28e23dcae270a
lutsd <lutsdm@gmail.com>
2024-10-29 22:34:39 +0100

    29.10.25 homework

---
+++ lesson_34/code/homework_34/event/model/Event.java
@@ -0,0 +1,79 @@
+package homework_34.event.model;
+
+import java.time.LocalDate;
+
+public class Event implements Comparable<Event>{
+
+    private int id;
+    private String title;
+    private String url;
+    private LocalDate date;
+
+    public Event(int id, String title, String url, LocalDate date) {
+        this.id = id;
+        this.title = title;
+        this.url = url;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Event event)) return false;
+
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Event{");
+        sb.append("id=").append(id);
+        sb.append(", title='").append(title).append('\'');
+        sb.append(", url='").append(url).append('\'');
+        sb.append(", date=").append(date);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public int compareTo(Event o) {
+        return this.getId() - o.getId();
+    }
+}
